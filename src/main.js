938804538635bb9874795d35eb1ee45408e6fb16
tomkp <tom@tomkp.com>
2018-11-03 14:55:02 +0000

    ...

--- src/main.js
+++ src/main.js
@@ -49,3 +49,45 @@ app.on('activate', function () {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+import { NFC } from 'nfc-pcsc';
+
+const nfc = new NFC(); // optionally you can pass logger
+
+nfc.on('reader', reader => {
+
+    console.log(`\n${reader.reader.name}  device attached`);
+
+    // needed for reading tags emulated with Android HCE
+    // custom AID, change according to your Android for tag emulation
+    // see https://developer.android.com/guide/topics/connectivity/nfc/hce.html
+    reader.aid = 'F222222222';
+
+    reader.on('card', card => {
+
+        // card is object containing following data
+        // [always] String type: TAG_ISO_14443_3 (standard nfc tags like Mifare) or TAG_ISO_14443_4 (Android HCE and others)
+        // [always] String standard: same as type
+        // [only TAG_ISO_14443_3] String uid: tag uid
+        // [only TAG_ISO_14443_4] Buffer data: raw data from select APDU response
+
+        console.log(`\n${reader.reader.name}  card detected`, card);
+
+    });
+
+    reader.on('card.off', card => {
+        console.log(`\n${reader.reader.name}  card removed`, card);
+    });
+
+    reader.on('error', err => {
+        console.log(`\n${reader.reader.name}  an error occurred`, err);
+    });
+
+    reader.on('end', () => {
+        console.log(`\n${reader.reader.name}  device removed`);
+    });
+
+});
+
+nfc.on('error', err => {
+    console.log('\nan error occurred', err);
+});
